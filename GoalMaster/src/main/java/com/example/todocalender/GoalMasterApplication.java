5584d9738cbe6c978036c601b7f2baedf46c84b4
Night-Swan <184144226+Night-Swan@users.noreply.github.com>
2025-10-18 11:25:23 +0200

    Created spring project in maven

---
+++ GoalMaster/src/main/java/com/example/todocalender/GoalMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.todocalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalMasterApplication.class, args);
+    }
+
+}
---
+++ GoalMaster/src/test/java/com/example/todocalender/GoalMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todocalender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
